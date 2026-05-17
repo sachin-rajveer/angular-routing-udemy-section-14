42cbd2940e9b4efa6c03f11b323d49de35e3fb1e
sachink <sachinkolekar@wegofin.com>
2026-05-17 18:39:03 +0530

    Alternative way: getting route parameter using ActivatedRoute.paramMap observable This is used in older angular versions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,5 +3,5 @@ import { provideRouter, withComponentInputBinding } from "@angular/router";
 import { appRoutes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(appRoutes, withComponentInputBinding())], 
+    providers: [provideRouter(appRoutes)], 
 }
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,6 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -9,9 +10,28 @@ import { UsersService } from '../users.service';
 })
 export class UserTasksComponent {
 
+  userName = '';
   userId = input.required<string>();
 
   private usersService = inject(UsersService);
+  private destroyRef = inject(DestroyRef);
 
-  userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name)
+  // userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name)
+
+  activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+      const subscription = this.activatedRoute.paramMap.subscribe({
+        next: (paramMap) => {
+          
+          this.userName = this.usersService.users.find( (u) => u.id === paramMap.get('userId')
+          )?.name || '';
+
+        }
+    })
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+
+  }
+
 }
