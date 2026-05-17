eb8510091d4c7af0a75646c457e7a8ab57255b02
sachink <sachinkolekar@wegofin.com>
2026-05-17 18:56:07 +0530

    Nested routes add children parameter to parent routes and define in array all routes add <router-outlet /> in parent component html file and import in parent component file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 export const appRoutes: Routes = [
         {
@@ -9,6 +11,16 @@ export const appRoutes: Routes = [
         },
         {
             path: 'users/:userId',
-            component: UserTasksComponent
+            component: UserTasksComponent,
+            children: [
+                {
+                    path: 'tasks', //http://<your-domain>/users/<uid>/tasks
+                    component: TasksComponent
+                },
+                {
+                    path: 'tasks/new', //http://<your-domain>/users/<uid>/tasks/new
+                    component: NewTaskComponent
+                }
+            ]
         }
     ];
