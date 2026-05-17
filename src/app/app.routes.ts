06c25b155630f1b73e816cf183d97377c240e6a8
sachink <sachinkolekar@wegofin.com>
2026-05-17 23:27:30 +0530

    to stay on same component use routerLink="./"
    to add Query parameters in url use
    [queryParams]="{'order': order() === 'asc' ? 'desc' : 'asc'}"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { userRoutes } from "./users/user.routes";
 
